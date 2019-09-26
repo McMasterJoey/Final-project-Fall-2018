b03621674f96884f08f18b871c6f3dfcd98a786f
JCSCMaster <jmcmaster@email.arizona.edu>
2019-09-26 16:36:04 -0700

    Revert "Merging Current inprogress files for GUI"
    This reverts commit d6a748e5f3159bd9339a8b22da68f5a0bd1fc1e9.

--- src/main/java/Gamejam/Gamejam.java
+++ src/main/java/Gamejam/Gamejam.java
@@ -18,16 +18,14 @@ public class Gamejam extends Application {
 	public static void main(String args[]) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Game Jam");
 		BorderPane basepane = new BorderPane();
-		basepane.setCenter(new GamejamMainScreen());
-		//basepane.setCenter(new TicTacToeControllerView());
-		Scene scene = new Scene(basepane, 800, 800);
+		basepane.setCenter(new TicTacToeControllerView());
+		Scene scene = new Scene(basepane, 1000, 1000);
 		stage.setScene(scene);
 		stage.show();
-		
 	}
 }
