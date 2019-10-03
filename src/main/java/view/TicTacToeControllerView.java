758dd2321cb678394f5312ed827dfdad4b08ec38
WesRodgers <wesrod@gmail.com>
2019-10-02 17:45:44 -0700

    Hacky, but it works

--- src/main/java/view/TicTacToeControllerView.java
+++ src/main/java/view/TicTacToeControllerView.java
@@ -5,11 +5,13 @@ import java.util.Observer;
 
 import java.awt.Point;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.AudioClip;
-import javafx.scene.paint.Color;
 import model.EasyAI;
 import model.TicTacToeModel;
 
@@ -20,17 +22,19 @@ import model.TicTacToeModel;
  * @author Wes Rodgers
  *
  */
-public class TicTacToeControllerView extends Canvas implements Observer {
+public class TicTacToeControllerView extends GridPane implements Observer {
 
     private TicTacToeModel gameModel;
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
-    private GraphicsContext gc;
     private Image xImage, yImage;
+    private BorderPane topLeft, midLeft, botLeft, topCenter, midCenter, botCenter,
+		topRight, midRight, botRight;
     private AudioClip moveSound;
     private AudioClip winSound;
     private AudioClip loseSound;
     private AudioClip tieSound;
+    private BorderPane[][] placeholder;
 
     public TicTacToeControllerView() {
         gameModel = new TicTacToeModel();
@@ -59,7 +63,6 @@ public class TicTacToeControllerView extends Canvas implements Observer {
         this.setWidth(WIDTH);
         this.setHeight(HEIGHT);
         setupResources();
-        setupListeners();
         setupBoard();
     }
 
@@ -80,42 +83,65 @@ public class TicTacToeControllerView extends Canvas implements Observer {
      * current moves to it.
      */
     private void setupBoard() {
-        gc = this.getGraphicsContext2D();
-        gc.setLineWidth(5);
-        drawBoard();
-    }
-
-    /**
-     * draws the move at position (row,col) to board
-     * 
-     * @param row the row the move is in
-     * @param col the column the move is in
-     * @param c   the move, 'X' or 'O'
-     */
-    private void drawMove(int row, int col, char c) {
-        Image drawImage = c == 'X' ? xImage : yImage;
-        gc.drawImage(drawImage, 0, 0, drawImage.getWidth(),
-                drawImage.getHeight(), row * 200 + 10, col * 200 + 10, 180,
-                180);
-    }
-
-    /**
-     * Draws the 4 lines that make up the board and checks
-     * the moves on the game model and draws those.
-     */
-    private void drawBoard() {
-        gc.strokeLine(200, 0, 200, 600);
-        gc.strokeLine(400, 0, 400, 600);
-        gc.strokeLine(0, 200, 600, 200);
-        gc.strokeLine(0, 400, 600, 400);
-        char[][] currState = gameModel.getBoard();
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (currState[row][col] != '_') {
-                    drawMove(col, row, currState[row][col]);
-                }
-            }
-        }
+    	placeholder = new BorderPane[3][3];
+    	
+        this.setAlignment(Pos.CENTER);
+        topLeft = new BorderPane();
+        topLeft.setStyle("-fx-border-color : white black black white; -fx-border-width : 2");
+        topLeft.setPrefSize(200, 200);
+        this.add(topLeft, 0, 0);
+        placeholder[0][0] = topLeft;
+        
+        midLeft = new BorderPane();        
+        midLeft.setStyle("-fx-border-color : black black black white; -fx-border-width : 2");
+        midLeft.setPrefSize(200, 200);
+        this.add(midLeft, 0, 1);
+        placeholder[0][1] = midLeft;
+        
+        botLeft = new BorderPane();        
+        botLeft.setStyle("-fx-border-color : black black white white; -fx-border-width : 2");
+        botLeft.setPrefSize(200, 200);
+        this.add(botLeft, 0, 2);
+        placeholder[0][2] = botLeft;
+        
+        topCenter = new BorderPane();        
+        topCenter.setStyle("-fx-border-color : white black black black; -fx-border-width : 2");
+        topCenter.setPrefSize(200, 200);
+        this.add(topCenter, 1, 0);
+        placeholder[1][0] = topCenter;
+        
+        midCenter = new BorderPane();        
+        midCenter.setStyle("-fx-border-color : black black black black; -fx-border-width : 2");
+        midCenter.setPrefSize(200, 200);
+        this.add(midCenter, 1, 1);
+        placeholder[1][1] = midCenter;
+        
+        botCenter = new BorderPane();        
+        botCenter.setStyle("-fx-border-color : black black white black; -fx-border-width : 2");
+        botCenter.setPrefSize(200, 200);
+        this.add(botCenter, 1, 2);
+        placeholder[1][2] = botCenter;
+        
+        topRight = new BorderPane();        
+        topRight.setStyle("-fx-border-color : white white black black; -fx-border-width : 2");
+        topRight.setPrefSize(200, 200);
+        this.add(topRight, 2, 0);
+        placeholder[2][0] = topRight;
+        
+        midRight = new BorderPane();        
+        midRight.setStyle("-fx-border-color : black white black black; -fx-border-width : 2");
+        midRight.setPrefSize(200, 200);
+        this.add(midRight, 2, 1);
+        placeholder[2][1] = midRight;
+        
+        botRight = new BorderPane();        
+        botRight.setStyle("-fx-border-color : black white white black; -fx-border-width : 2");
+        botRight.setPrefSize(200, 200);
+        this.add(botRight, 2, 2);
+        placeholder[2][2] = botRight;
+        
+        setupListeners();
+        
     }
 
     /**
@@ -123,13 +149,40 @@ public class TicTacToeControllerView extends Canvas implements Observer {
      * clicked on and attempts to make a move at that spot.
      */
     private void setupListeners() {
-        this.setOnMouseClicked((click) -> {
-            int x = (int) click.getX() / 200;
-            int y = (int) click.getY() / 200;
-            if(gameModel.available(y, x)) {
-                gameModel.humanMove(y, x, false);
-                moveSound.play();
-            }
+        topLeft.setOnMouseClicked((click)->{
+        	gameModel.humanMove(0, 0, false);
+        });
+        
+        midLeft.setOnMouseClicked((click)->{
+        	gameModel.humanMove(1, 0, false);
+        });
+        
+        botLeft.setOnMouseClicked((click)->{
+        	gameModel.humanMove(2, 0, false);
+        });
+        
+        topCenter.setOnMouseClicked((click)->{
+        	gameModel.humanMove(0, 1, false);
+        });
+        
+        midCenter.setOnMouseClicked((click)->{
+        	gameModel.humanMove(1, 1, false);
+        });
+        
+        botCenter.setOnMouseClicked((click)->{
+        	gameModel.humanMove(2, 1, false);
+        });
+        
+        topRight.setOnMouseClicked((click)->{
+        	gameModel.humanMove(0, 2, false);
+        });
+        
+        midRight.setOnMouseClicked((click)->{
+        	gameModel.humanMove(1, 2, false);
+        });
+        
+        botRight.setOnMouseClicked((click)->{
+        	gameModel.humanMove(2, 2, false);
         });
     }
 
@@ -139,8 +192,23 @@ public class TicTacToeControllerView extends Canvas implements Observer {
      */
     @Override
     public void update(Observable arg0, Object arg1) {
-        gc.clearRect(0, 0, WIDTH, HEIGHT);
-        drawBoard();
+    	moveSound.play();
+    	char [][] board = gameModel.getBoard();
+    	for(int i=0; i<3; i++) {
+        	for(int j=0; j<3; j++) {
+        		if(board[i][j] != '_') {
+        			Image image = board[i][j] == 'X' ? xImage : yImage;        			
+        			ImageView imageView = new ImageView(image);
+        			imageView.setFitHeight(196);
+        			imageView.setFitWidth(196);
+        			HBox box = new HBox();
+        			box.getChildren().add(imageView);
+        			box.setStyle(placeholder[j][i].getStyle());
+        			this.getChildren().remove(placeholder[j][i]);
+        			this.add(box, j, i);
+        		}
+        	}
+        }
         System.out.println(gameModel.toString());
         if(gameModel.tied()) {
             tieSound.play();
@@ -154,31 +222,31 @@ public class TicTacToeControllerView extends Canvas implements Observer {
             }
             String winningDirection = gameModel.getWinningDirection();
             Point[] winningSquares = gameModel.getWinningSquares(winningDirection);
-            gc.setStroke(Color.BLUE);
+            //gc.setStroke(Color.BLUE);
             
             switch(winningDirection) {
             case "horizontal":
-                gc.strokeLine(0, winningSquares[0].x*200+100, 600, winningSquares[2].x*200+100);
+                //gc.strokeLine(0, winningSquares[0].x*200+100, 600, winningSquares[2].x*200+100);
                 System.out.println("horizontal " + winningSquares[0].toString());
                 System.out.println(winningSquares[1].toString());
                 System.out.println(winningSquares[2].toString());
                 break;
             case "vertical":
-                gc.strokeLine(winningSquares[0].y*200+100, 0, winningSquares[2].y*200+100, 600);
+                //gc.strokeLine(winningSquares[0].y*200+100, 0, winningSquares[2].y*200+100, 600);
                 System.out.println("vertical " + winningSquares[0].toString());
                 System.out.println(winningSquares[1].toString());
                 System.out.println(winningSquares[2].toString());
                 break;
             case "diagonal":
                 if(winningSquares[0].x == 0) {
-                    gc.strokeLine(0, 0, 600, 600);
+                    //gc.strokeLine(0, 0, 600, 600);
                 }
                 else {
-                    gc.strokeLine(0, 600, 600, 0);
+                    //gc.strokeLine(0, 600, 600, 0);
                 }
                 break;
             }
-            gc.setStroke(Color.BLACK);
+            //gc.setStroke(Color.BLACK);
         }
     }
 
