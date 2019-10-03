97fd54032f42f476575d91868627124c7cf0f9a3
Joey McMaster <jmcmaster@email.arizona.edu>
2019-10-03 13:44:32 -0700

    Added additional funcitonality to the GameButton class along with documentation.

--- src/main/java/Gamejam/GameButton.java
+++ src/main/java/Gamejam/GameButton.java
@@ -8,13 +8,36 @@ import javafx.scene.control.Button;
  */
 public class GameButton extends Button {
 	private String metadata = "";
+	private int metadataint = 0;
 	public GameButton() {
 		super();
 	}
+	/**
+	 * Sets meta data in the form of a string for this button.
+	 * @param data The meta data to be set
+	 */
 	public void setMetaDataString(String data) {
 		this.metadata = data;
 	}
+	/**
+	 * Sets meta data in the from of a int for this button.
+	 * @param data The meta data to be set
+	 */
+	public void setMetaDataInt(int data) {
+		this.metadataint = data;
+	}
+	/**
+	 * Fetches the string meta data stored for this button.
+	 * @return The string that was stored.
+	 */
 	public String getMetaDataString() {
 		return this.metadata;
+	}
+	/**
+	 * Fetches the int meta data stored for this button.
+	 * @return The int that was stored.
+	 */
+	public int getMetaDataInt() {
+		return this.metadataint;
 	}
 }
