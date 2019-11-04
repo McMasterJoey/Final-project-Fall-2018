e17cdc4714a1587bf16be58ac7465957a70512bf
WesRodgers <wesrod@gmail.com>
2019-11-04 15:02:08 -0700

    stat logging for BS

--- src/main/java/battleship/BattleshipControllerView.java
+++ src/main/java/battleship/BattleshipControllerView.java
@@ -666,8 +666,10 @@ public class BattleshipControllerView extends GameControllerView {
 
 			if (gameModel.won(true)) {
 				winSound.play();
+				statsManager.logGameStat("Tic-Tac-Toe",  LogStatType.WIN, 1, getScore());
 			} else {
 				loseSound.play();
+				statsManager.logGameStat("Tic-Tac-Toe",  LogStatType.LOSS, 1, getScore());
 			}
 		}
 		if (shipsSet) {
